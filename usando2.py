ee6d614f6ddbef0cbe4037c964e912695e6f8009
JfRes <juanfranciscocastulo@gmail.com>
2021-05-12 20:19:26 +0200

    Add files via upload

---
+++ usando2.py
@@ -0,0 +1,16 @@
+from imageai.Prediction.Custom import CustomImagePrediction
+
+prediction = CustomImagePrediction()
+prediction.setModelTypeAsResNet()
+modelo="imagenes\models\model_ex-049_acc-0.906250.h5"
+prediction.setModelPath(modelo)
+prediction.setJsonPath("imagenes\json\model_class.json")
+prediction.loadModel(num_objects=2)
+def inicia():
+    foto=r"estampado.webp"
+
+    predictions, probabilities = prediction.predictImage(foto, result_count=2)
+
+    for eachPrediction, eachProbability in zip(predictions, probabilities):
+        print(eachPrediction , " : " , eachProbability)
+inicia()
